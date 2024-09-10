674481e0d4e474401938c67fb2da49dc4c8d8dd1
zeeshanfaizan <159419943+zeeshanfaizan@users.noreply.github.com>
2024-09-10 11:32:07 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Simple Hello World Node.js app
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://localhost:8080/`);
+});
